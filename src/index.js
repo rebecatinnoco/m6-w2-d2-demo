a2da84d92cfca3ac6b0e80c734bbe7ac9a9a4497
rebecatinnoco <104739229+rebecatinnoco@users.noreply.github.com>
2022-10-05 22:35:55 -0400

    commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import './api/server'
+import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
+
+store.dispatch(fetchTodos())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
